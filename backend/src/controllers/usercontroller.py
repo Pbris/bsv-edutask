ba0b33f495ea3c0d1ab1a1ff9e3730484ef9061e
airhelios <owais.sulehria@gmail.com>
2025-05-16 12:55:56 +0200

    Ugly fixes so that the changes show

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -32,7 +32,7 @@ class UserController(Controller):
             users = self.dao.find({'email': email})
             if len(users) == 1:
                 return users[0]
-            elif len(users) == 0:
+            elif len(users) == 0: ##Fixed
                 return None
             else:
                 print(f'Error: more than one user found with mail {email}')
